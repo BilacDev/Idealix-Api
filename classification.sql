5ca8943e47727a33e91a23e521a01282ebfb97e7
riwersxd <guilherme.rc.98@gmail.com>
2019-10-01 21:16:29 -0300

    Limpa script do banco

---
+++ classification.sql
@@ -0,0 +1,4 @@
+insert into responsible(name, email, password)
+values ('Guilherme Rios', 'guilherme.rc.98@gmail.com', 'teste');
+
+drop database `idealix_database`;
--- idealix-database.sql
+++ idealix-database.sql
@@ -1,30 +1,24 @@
-CREATE DATABASE  IF NOT EXISTS `idealix` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `idealix`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: idealix
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.22-MariaDB
+CREATE DATABASE IF NOT EXISTS `idealix_database`;
+USE `idealix_database`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE IF NOT EXISTS `responsible` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `name` varchar(180) NOT NULL,
+   `email` varchar(180) NOT NULL,
+   `password` varchar(60) NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `child`
---
+CREATE TABLE IF NOT EXISTS `classification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `age` int(11) NOT NULL,
+  `height` decimal(3,2) NOT NULL,
+  `weight` decimal(5,2) NOT NULL,
+  `gender` enum('f','m') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `child`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `child` (
+CREATE TABLE IF NOT EXISTS `child` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `birthday` date NOT NULL,
@@ -34,107 +28,18 @@ CREATE TABLE `child` (
   PRIMARY KEY (`id`),
   KEY `fk_child_responsible_idx` (`responsible_id`),
   KEY `fk_child_classification1_idx` (`classification_id`),
-  CONSTRAINT `fk_child_classification1` FOREIGN KEY (`classification_id`) REFERENCES `classification` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_child_responsible` FOREIGN KEY (`responsible_id`) REFERENCES `responsible` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  CONSTRAINT `fk_child_classification` FOREIGN KEY (`classification_id`) REFERENCES `classification` (`id`),
+  CONSTRAINT `fk_child_responsible` FOREIGN KEY (`responsible_id`) REFERENCES `responsible` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `child`
---
-
-LOCK TABLES `child` WRITE;
-/*!40000 ALTER TABLE `child` DISABLE KEYS */;
-/*!40000 ALTER TABLE `child` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `classification`
---
-
-DROP TABLE IF EXISTS `classification`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `classification` (
+CREATE TABLE IF NOT EXISTS `history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `age` int(11) NOT NULL,
   `height` decimal(3,2) NOT NULL,
   `weight` decimal(5,2) NOT NULL,
-  `gender` enum('f','m') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `classification`
---
-
-LOCK TABLES `classification` WRITE;
-/*!40000 ALTER TABLE `classification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `classification` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `history`
---
-
-DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `age` int(11) NOT NULL,
-  `height` decimal(3,2) NOT NULL,
-  `weight` decimal(5,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `child_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_history_child1_idx` (`child_id`),
-  CONSTRAINT `fk_history_child1` FOREIGN KEY (`child_id`) REFERENCES `child` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `history`
---
-
-LOCK TABLES `history` WRITE;
-/*!40000 ALTER TABLE `history` DISABLE KEYS */;
-/*!40000 ALTER TABLE `history` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `responsible`
---
-
-DROP TABLE IF EXISTS `responsible`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `responsible` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `responsible`
---
-
-LOCK TABLES `responsible` WRITE;
-/*!40000 ALTER TABLE `responsible` DISABLE KEYS */;
-/*!40000 ALTER TABLE `responsible` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-17 10:34:57
+  KEY `fk_history_child_idx` (`child_id`),
+  CONSTRAINT `fk_history_child` FOREIGN KEY (`child_id`) REFERENCES `child` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
